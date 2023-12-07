2e75e17c95ca5b31f045120217a0383c1a82f02d
Claire Fisher <cafisher83@yahoo.co.uk>
2023-12-07 18:30:13 +0000

    chore: write pseudocode for game backend

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 console.log("Js connected properly");
+
+// event listener: Users weapon choice //
+
+// hide game main section, show countdown timer circles //
+
+// function randomise Ai weapon choice //
+
+// game rules object with win/lose combos. Compare user & Ai weapons //
+
+// hide countdown timer circles section, show games results section //
+
+// show new scores tally in scores section //
+
+// event listener: play again  //
